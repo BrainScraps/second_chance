7794ca74db63db94625ce614f9cc14de05c5b65f
Isaac Elias <isaac@milyoni.com>
2014-02-08 15:30:37 -0800

    add leaflet

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SecondChance::Application.config.secret_key_base = '020310fe665868434bbbba49829e4d6bc7f42df27ff3101d01eba6ea2273f6bd886541ae26f59eeb0fabcade149c26e180cb7326058b690252d3267a54aacd58'
